3192a03478eda87913d2ee363d442d7e8e143980
AntonZhidkov <ayzdkv@gmail.com>
2021-03-21 14:00:30 +0300

    mesto_ver1.4

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,46 @@
 let popup = document.querySelector('.popup');
+let popupForm = document.querySelector('.popup__edit-form');
 let openPopupBtn = document.querySelector('.profile__edit-button');
 let closePopupBtn = document.querySelector('.popup__close-button');
 let savePopupBtn = document.querySelector('.popup__save-button');
+let popupName = document.querySelector('.popup__input_type_name');
+let popupOccupation = document.querySelector('.popup__input_type_occupation');
 let profileName = document.querySelector('.profile__name');
 let profileOccupation = document.querySelector('.profile__occupation');
 
 
 function openPopup () {
     popup.classList.add('popup_visible');
-}
+    popupName.value = profileName.textContent;
+    popupOccupation.value = profileOccupation.textContent;
+};
 
 function closePopup () {
     popup.classList.remove('popup_visible');
-}
+};
 
-openPopupBtn.addEventListener('click', function() {
-    openPopup ();
-});
-
-closePopupBtn.addEventListener('click', function() {
-    closePopup ();
-});
-
-savePopupBtn.addEventListener('click', function() {
-    editProfile ();
-});
-
+/*before review...
 function editProfile () {
-    let popupName = document.querySelector('.popup__input_type_name');
-    let popupOccupation = document.querySelector('.popup__input_type_occupation');
+    profileName.textContent = popupName.value;
+    profileOccupation.textContent = popupOccupation.value;
+    closePopup()
+};*/
+
+/*after review...*/
+function formSubmitHandler (evt) {
+    evt.preventDefault();
     profileName.textContent = popupName.value;
     profileOccupation.textContent = popupOccupation.value;
     closePopup()
 };
+
+openPopupBtn.addEventListener('click', openPopup);
+
+closePopupBtn.addEventListener('click', closePopup);
+
+/*before review...
+savePopupBtn.addEventListener('click', editProfile);*/
+
+/*after review...*/
+popupForm.addEventListener('submit', formSubmitHandler);
+
